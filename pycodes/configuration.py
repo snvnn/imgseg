61213b6e7918bbd2d75e9d1f7181f2546b94d091
YoonHyeon <yunh1205@gmail.com>
2025-11-18 18:52:12 +0900

    minor update

--- pycodes/configuration.py
+++ pycodes/configuration.py
@@ -17,8 +17,8 @@ MAP_PATH = 'train/trimaps'
 
 # Learning parameters
 BATCH_SIZE = 16
-LEARNING_RATE = 0.0001
-EPOCHS = 1500
+LEARNING_RATE = 0.001
+EPOCHS = 30
 MIN_DELTA = 1e-4  # 개선으로 인정할 최소 손실 감소량
 BEST_LOSS = float('inf')
 PATIENCE = -1     # PATIENCE > 0 일 때 Early Stop 동작
